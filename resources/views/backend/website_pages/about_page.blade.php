8b7e4c3dbc116c8f01aa39f71dace54b95916160
ehsan ul haq <ehsanulhaq11199@gmail.com>
2023-03-17 01:12:25 +0500

    New Theme integration
    - Contact us page
    - about page complete but functionality is remaining

--- resources/views/backend/website_pages/about_page.blade.php
+++ resources/views/backend/website_pages/about_page.blade.php
@@ -1,9 +1,176 @@
-@extends('layouts.master_backend')
+@extends('layouts.master_backend_new')
 @section('content')
   @php
     $countFeatures = ($data_row->about_section_features!=null) ? 1 : 0;
     $featuresDecodeJson = json_decode($data_row->about_section_features);
   @endphp
+<!-- 
+  <section class="">
+                    <div class="row">
+                        < !-- Invoice repeater - ->
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">Invoice</h4>
+                                </div>
+                                <div class="card-body">
+                                    <form action="#" class="invoice-repeater">
+                                        <div data-repeater-list="invoice">
+                                            <div data-repeater-item>
+                                                <div class="row d-flex align-items-end">
+                                                    <div class="col-md-4 col-12">
+                                                        <div class="mb-1">
+                                                            <label class="form-label" for="itemname">Item Name</label>
+                                                            <input type="text" class="form-control" id="itemname" aria-describedby="itemname" placeholder="Vuexy Admin Template" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-2 col-12">
+                                                        <div class="mb-1">
+                                                            <label class="form-label" for="itemcost">Cost</label>
+                                                            <input type="number" class="form-control" id="itemcost" aria-describedby="itemcost" placeholder="32" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-2 col-12">
+                                                        <div class="mb-1">
+                                                            <label class="form-label" for="itemquantity">Quantity</label>
+                                                            <input type="number" class="form-control" id="itemquantity" aria-describedby="itemquantity" placeholder="1" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-2 col-12">
+                                                        <div class="mb-1">
+                                                            <label class="form-label" for="staticprice">Price</label>
+                                                            <input type="text" readonly class="form-control-plaintext" id="staticprice" value="$32" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-2 col-12 mb-50">
+                                                        <div class="mb-1">
+                                                            <button class="btn btn-outline-danger text-nowrap px-1" data-repeater-delete type="button">
+                                                                <i data-feather="x" class="me-25"></i>
+                                                                <span>Delete</span>
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <hr />
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <button class="btn btn-icon btn-primary" type="button" data-repeater-create>
+                                                    <i data-feather="plus" class="me-25"></i>
+                                                    <span>Add New</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <! -- /Invoice repeater -- >
+                    </div>
+                </section>
+ -->
+
+
+
+<section>
+  <div class="row">
+  {{Form::model($data_row,['route'=>'update-contact-page','id'=>'contact-page-form','files'=>true])}}
+      <div class="col-12">
+          <div class="card">
+              <div class="card-header border-bottom">
+                  <h4 class="card-title">Banner Image</h4>
+              </div>
+              <div class="card-body">
+                  <div class="row">
+                        <div class="col-xl-4 col-md-6 col-12">
+                            <div class="mb-1">
+                              <label>Image</label>
+                              <input type="file" name="banner_image" class="form-control">
+                              @if($data_row->about_section_banner!='' && $data_row->about_section_banner!=null)
+                                  <img height="150" width="50%" src="{{checkFile($data_row->about_section_banner,'uploads/banners/','no-img.png')}}"/>
+                                @endif
+                            </div>
+                        </div>
+            
+                 
+                  </div>
+              
+              <hr />
+              <div class="card-header border-bottom">
+                  <h4 class="card-title">Introduction</h4>
+              </div>
+              <div class="card-body">
+              <div class="row">
+                        <div class="col-6">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Tagline</label>
+                                
+                                {{ Form::text('about_section_tagline',null,['class'=>'form-control', 'placeholder'=>'Enter Tagline']) }}
+                            </div>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Heading</label>
+                                
+                                {{ Form::text('about_section_heading',null,['class'=>'form-control', 'placeholder'=>'Enter Heading']) }}
+                            </div>
+                        </div>
+
+                        <div class="col-xl-12 col-md-12 col-12">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Description</label>
+                                {{ Form::textarea('about_section_desc',null,['class'=>'form-control', 'placeholder'=>'Enter Description','rows'=>2]) }}
+                                
+                            </div>
+                        </div>
+
+                        <div class="col-xl-4 col-md-6 col-12">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Image</label>
+                                <input type="file" name="about_section_image" class="form-control">
+                                <img width="100" src="{{checkFile($data_row->about_section_image,'uploads/about_us/','no-img.png')}}"/>
+                            </div>
+                        </div>
+                      </div>
+
+
+                      <hr />
+              <div class="card-header border-bottom">
+                  <h4 class="card-title">Features</h4>
+              </div>
+                <div class="card-body">
+                  <div class="row">
+
+
+                  </div>
+                </div>
+
+
+
+
+                      <div class="col-xl-4 col-md-6 col-12">
+                          <button type="submit" class="btn btn-primary" name="submit" value="Submit">{{lang_trans('btn_submit')}}</button>
+                      </div>
+              </div>
+          </div>
+      </div>
+      
+          </div>
+      </div>
+      {{ Form::close() }}
+  </div>
+
+</section>
+
+
+<!-- 
+old code
       <div class="row">
         {{Form::model($data_row,['route'=>'update-about-page','id'=>'home-page-form','files'=>true])}}
         <div class="col-md-6">
@@ -27,7 +194,7 @@
         </div>
 
         <div class="col-md-6">
-           <!-- ==========* Start AboutUs Section *========== -->
+     
           <div class="x_panel">
             <div class="x_title">
               <h2>Introduction</h2>
@@ -93,7 +260,7 @@
                 </div>
               </div>
           </div>
-          <!-- ==========* End AboutUs Section *========== -->
+     
         </div>
 
         <div class="col-md-12 text-right">
@@ -102,7 +269,6 @@
         {{ Form::close() }}
       </div>
 
-<!-- ==========* Start Clone Elements Section *========== -->
 
   <div class="clone_features_elem hide_elem">
     <div class="row features-row border-btm pad-top-10">
@@ -156,13 +322,19 @@
       <button type="button" class="btn btn-danger delete-row-ourteam"><i class="fa fa-minus"></i></button><br/>
     </div>
     </div>
-  </div>
+  </div> -->
 
 <!-- ==========* End Clone Elements Section *========== -->
 {{-- require set var in js var --}}
-  <script>
+  <!-- <script>
     globalVar.page = 'website_about_page';
     globalVar.featuresCount = {{$countFeatures}};
   </script>
-  <script type="text/javascript" src="{{URL::asset('public/js/page_js/page.js?v='.rand(1111,9999).'')}}"></script>
+  <script type="text/javascript" src="{{URL::asset('public/js/page_js/page.js?v='.rand(1111,9999).'')}}"></script> -->
+@endsection
+@section('scripts')
+<!-- BEGIN: Page JS-->
+  <script src="{{URL::asset('public/app-assets/vendors/js/forms/repeater/jquery.repeater.min.js')}}"></script>
+  <script src="{{URL::asset('public/app-assets/js/scripts/forms/form-repeater.js')}}"></script>
+<!-- END: Page JS-->
 @endsection
--- resources/views/backend/website_pages/contact_page.blade.php
+++ resources/views/backend/website_pages/contact_page.blade.php
@@ -1,5 +1,96 @@
-@extends('layouts.master_backend')
+@extends('layouts.master_backend_new')
 @section('content')
+
+<section>
+  <div class="row">
+  {{Form::model($data_row,['route'=>'update-contact-page','id'=>'contact-page-form','files'=>true])}}
+      <div class=" col-6">
+          <div class="card">
+              <div class="card-header border-bottom">
+                  <h4 class="card-title">Banner Image</h4>
+              </div>
+              <div class="card-body">
+
+                  <div class="">
+                      <label>Image</label>
+                      <input type="file" name="banner_image" class="form-control">
+                    </div>
+                    @if($data_row->banner_image!='' && $data_row->banner_image!=null)
+                      <img height="150" width="50%" src="{{checkFile($data_row->banner_image,'uploads/banners/','no-img.png')}}"/>
+                    @endif
+              </div>
+          </div>
+      </div>
+      <div class=" col-12">
+          <div class="card">
+              <div class="card-header border-bottom">
+                  <h4 class="card-title">Contact Us</h4>
+              </div>
+              <div class="card-body">
+                     <div class="row">
+                        <div class="col-6">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Tagline</label>
+                                
+                                {{ Form::text('tagline',null,['class'=>'form-control', 'placeholder'=>'Enter Tagline']) }}
+                            </div>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Heading</label>
+                                
+                                {{ Form::text('heading',null,['class'=>'form-control', 'placeholder'=>'Enter Heading']) }}
+                            </div>
+                        </div>
+
+                        <div class="col-xl-4 col-md-6 col-12">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Facebook Link</label>
+                                {{ Form::text('facebook_link',null,['class'=>'form-control', 'placeholder'=>'Enter Facebook Link']) }}
+                                
+                            </div>
+                        </div>
+
+                        <div class="col-xl-4 col-md-6 col-12">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Twitter Link</label>
+                                {{ Form::text('twitter_link',null,['class'=>'form-control', 'placeholder'=>'Enter Twitter Link']) }}
+                                
+                            </div>
+                        </div>
+
+                        <div class="col-xl-4 col-md-6 col-12">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Instagram Link</label>
+                                {{ Form::text('instagram_link',null,['class'=>'form-control', 'placeholder'=>'Enter Instagram Link']) }}
+                                
+                            </div>
+                        </div>
+
+                        <div class="col-xl-4 col-md-6 col-12">
+                            <div class="mb-1">
+                                <label class="form-label" for="basic-default-name">Linkedin Link</label>
+                                {{ Form::text('linkedin_link',null,['class'=>'form-control', 'placeholder'=>'Enter Linkedin Link']) }}
+                                
+                            </div>
+                        </div>
+                        
+
+                      </div>
+                      <div class="col-xl-4 col-md-6 col-12">
+                          <button type="submit" class="btn btn-primary" name="submit" value="Submit">{{lang_trans('btn_submit')}}</button>
+                        </div>
+              </div>
+          </div>
+      </div>
+      {{ Form::close() }}
+  </div>
+
+</section>
+
+
+<!--
       <div class="row">
         {{Form::model($data_row,['route'=>'update-contact-page','id'=>'contact-page-form','files'=>true])}}
         <div class="col-md-6">
@@ -23,7 +114,7 @@
         </div>
 
         <div class="col-md-6">
-          <!-- ==========* Start Services Section *========== -->
+          <! -- ==========* Start Services Section *========== -- >
           <div class="x_panel">
             <div class="x_title">
               <h3 class="box-title">Contact Us</h3>
@@ -52,7 +143,7 @@
                   </div>
               </div>
           </div>
-          <!-- ==========* End Services Section *========== -->
+          < !-- ==========* End Services Section *========== -- >
         </div>
         
         <div class="col-md-12 text-right">
@@ -60,5 +151,5 @@
         </div>
         {{ Form::close() }}
       </div>
-
+-->
 @endsection
